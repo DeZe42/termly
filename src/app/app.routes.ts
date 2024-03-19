eeda47743008bfc308133fa7694cd831c11f289f
zsolt.denes <zsolt.denes@trendency.hu>
2024-03-19 14:49:40 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'page_1',
+    loadComponent: () =>
+      import('./page-1/page-1.component').then((mod) => mod.Page1Component),
+  },
+  {
+    path: 'page_2',
+    loadComponent: () =>
+      import('./page-2/page-2.component').then((mod) => mod.Page2Component),
+  },
+];
